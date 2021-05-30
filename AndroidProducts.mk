b34a38340ee67c70f21a3e17e9141036cac862d0
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-05-30 18:01:58 -0400

    Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kltechnduo.mk
+    $(LOCAL_DIR)/havoc_kltechnduo.mk
--- havoc_kltechnduo.mk
+++ havoc_kltechnduo.mk
@@ -15,10 +15,10 @@
 # limitations under the License.
 #
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/kltechnduo/full_kltechnduo.mk)
 
 PRODUCT_DEVICE := kltechnduo
-PRODUCT_NAME := lineage_kltechnduo
+PRODUCT_NAME := havoc_kltechnduo
